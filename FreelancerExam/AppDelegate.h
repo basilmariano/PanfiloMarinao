04b6c4197eff29f6c8e322c5a2f7db96b594f781
basilmariano <panfilomariano@gmail.com>
2015-07-10 09:37:50 +0800

    Initial Commit

---
+++ FreelancerExam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreelancerExam
+//
+//  Created by Sil Piplay on 7/10/15.
+//  Copyright (c) 2015 Panfilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FreelancerExam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FreelancerExam
+//
+//  Created by Sil Piplay on 7/10/15.
+//  Copyright (c) 2015 Panfilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
